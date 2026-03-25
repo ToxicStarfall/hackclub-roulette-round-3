a65fc2db0c92d3d151543fdd00b718daf2c7098f
ToxicStarfall <mr.dumbosayshi@gmail.com>
2026-03-25 09:26:14 -0500

    Moved and renamed files

---
+++ events/event.gd
@@ -0,0 +1,21 @@
+class_name Event
+extends Resource
+
+#signal dialogue_progressed
+
+#@export var type: Types = EventTypes.NORMAL
+
+var id: String
+@export var title: String = ""
+#@export var texture: Texture2D
+#@export var description: String = ""
+#@export var dialogue: DialogueResource
+
+@export_category("Configuration")
+#@export var regions: Array[Region]
+
+
+#func exit():
+	# Do stuff after event ends
+	#   Apply effects, etc...
+	#event_exited.emit()
