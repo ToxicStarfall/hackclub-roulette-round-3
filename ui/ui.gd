d3ccfbd000faf136db580615cd4811d82cde5273
ToxicStarfall <mr.dumbosayshi@gmail.com>
2026-08-15 18:29:54 -0500

    UI singleton
    Update project.godot

--- ui/ui.gd
+++ ui/ui.gd
@@ -1,10 +1,54 @@
 extends Control
 
 
+const Scenes = {
+	# Characters
+	CHARACTER_CARD = preload("res://ui/components/character_card.tscn"),
+	CHARACTER_CARD_ENEMY = preload("res://ui/components/character_card_enemy.tscn"),
+	# Inventory
+	CONTEXT_MENU = preload("res://ui/context_menu.tscn"),
+	INVENTORY_SLOT = preload("res://ui/components/inventory/inv_slot.tscn"),
+	ITEM_TOOLTIP = preload("res://ui/item_tooltip.tscn"),
+	#STATUS_TOOLTIP = preload(""),
+}
+
+
+var popups: Array[Control]
+@onready var ui = get_tree().root.get_node("Main/UI")
+
+
 func _ready() -> void:
+	
 	# Initial screen setup
-	$SplashScreen.hide()
-	%MainMenu.show()
-	%GameScreen.hide()
-	$PauseMenu.hide()
-	pass
+	ui.get_node("SplashScreen").hide()
+	ui.get_node("MainMenu").show()
+	ui.get_node("GameScreen").hide()
+	ui.get_node("PauseMenu").hide()
+	
+	
+	var input_mask = Control.new()
+	input_mask.name = "InputCatcher"
+	input_mask.set_anchors_preset(Control.PRESET_FULL_RECT)
+	input_mask.z_index = 1
+	add_child(input_mask)
+	
+	input_mask.gui_input.connect( _on_input_mask_gui_input )
+		
+
+
+func _on_input_mask_gui_input(event: InputEvent):
+	if event is InputEventMouseButton:
+		clear_popups()
+
+
+
+func add_popup(node: Control):
+	popups.append(node)
+	ui.add_child(node)
+
+
+func clear_popups():
+	for popup in popups:
+		popups.erase(popup)
+		if !popup is FloatingPanelContainer:
+			popup.queue_free()
