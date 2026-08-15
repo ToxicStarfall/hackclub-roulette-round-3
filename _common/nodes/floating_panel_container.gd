e0beae9c490cc131fcae7fa35af5fe860eee64dc
ToxicStarfall <mr.dumbosayshi@gmail.com>
2026-08-15 18:36:50 -0500

    Custom floating panel node for tooltips

---
+++ _common/nodes/floating_panel_container.gd
@@ -0,0 +1,44 @@
+class_name FloatingPanelContainer
+extends PanelContainer
+
+
+@export var handle_size: float = 2.0
+@export var draggable: bool = true
+@export var resizeable: bool = true
+@export var limit_to_screen_bounds: bool = true
+
+var dragging: bool = false
+#var drag_start: Vector2
+var drag_offset: Vector2
+
+
+func _ready() -> void:
+	pass
+
+
+func _input(event: InputEvent) -> void:
+	# TODO - Fix input not propagating to both CloseButton and drag input.
+	
+	
+	if event is InputEventMouse:
+		if dragging:
+			global_position = get_global_mouse_position() - drag_offset
+			
+	#if event is InputEventMouseButton:
+		if Rect2( Vector2(), size).has_point( get_local_mouse_position() ):
+			if !Rect2( Vector2.ONE * handle_size, size - Vector2.ONE * (handle_size * 2)).has_point( get_local_mouse_position() ):
+				mouse_default_cursor_shape = Control.CURSOR_HSIZE
+				# TODO - Resizing code
+			else:
+				mouse_default_cursor_shape = Control.CURSOR_ARROW
+				
+				if draggable:
+					if event is InputEventMouseButton:
+						if event.pressed:
+							dragging = true
+							#drag_start = get_local_mouse_position()
+							drag_offset = get_local_mouse_position()
+							mouse_default_cursor_shape = Control.CURSOR_DRAG
+						else:
+							dragging = false
+							mouse_default_cursor_shape = Control.CURSOR_ARROW
--- ui/item_tooltip.gd
+++ ui/item_tooltip.gd
@@ -1,4 +1,5 @@
-extends PanelContainer
+#extends PanelContainer
+extends FloatingPanelContainer
 
 
 
@@ -6,8 +7,9 @@ func _ready() -> void:
 	%CloseButton.pressed.connect( queue_free )
 
 
-func set_item(item_id: StringName, _owned: int = 0):
-	var item: ItemData = Registries.ITEMS.load_entry(item_id)
+#func set_item(item_id: StringName, _owned: int = 0):
+func set_item(item: ItemData, _owned: int = 0):
+	#var item: ItemData = Registries.ITEMS.load_entry(item_id)
 	%ItemIcon.texture = item.icon
 	%ItemLabel.text = item.name
 	%ItemDescription.text = item.description
