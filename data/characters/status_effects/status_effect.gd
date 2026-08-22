bf9349c9d0bf7b478960049993e7504e6eb6c30b
ToxicStarfall <mr.dumbosayshi@gmail.com>
2026-08-22 09:39:01 -0500

    Status effects

--- data/characters/status_effects/status_effect.gd
+++ data/characters/status_effects/status_effect.gd
@@ -5,6 +5,14 @@ extends Resource
 @export var icon: Texture2D
 @export var name: String
 @export_multiline() var description: String
-@export var duration: float  ## Duration in in-game hours. -1 = infinite.
+@export var duration: float = 1.0  ## Duration in in-game hours. -1 means infinite duration.
+
+@export var attribute_effect: CharacterData.Attribute
+#@export var attribute_base_effect: float = 1.0
+@export var efficiency_effect: CharacterData.Efficiency
+#@export var efficiency_base_effect: float = 1.0
+
 #@export var effects: Array
 @export var levels: Array[StatusEffectLevel]
+
+var current_level
